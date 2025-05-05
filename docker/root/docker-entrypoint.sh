d52365027d1498fe9aa57beaca9e67fa1f83010e
FMotalleb <m.falahnejad@dornica.net>
2025-05-05 11:17:19 +0330

    initialization of go project

--- docker/root/docker-entrypoint.sh
+++ docker/root/docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+
 input="$VLESS_PROXY"
 if [ -z "$input" ]; then
   echo "PLEASE SET A VLESS_PROXY ENV VARIABLE"
@@ -27,8 +28,19 @@ export TLS_ENABLED="$tls_on"
 export TRANSPORT="${type:-"ws"}"
 export TRANSPORT_PATH="${path:-'/'}"
 export ENCRYPTION="${encryption:-'none'}"
+packetEncoding=$(echo -n "${packetEncoding:-'xudp'}" | tr -d "'\"")
 export PACKET_ENCODING="${packetEncoding:-'xudp'}"
 
 envsubst </etc/singbox/config.json.template >/etc/singbox/config.json
+
+openssl req -x509 -nodes -days 365 \
+  -newkey rsa:2048 \
+  -keyout /etc/ssl/private/haproxy.key \
+  -out /etc/ssl/certs/haproxy.crt \
+  -subj "/CN=*"
+
+mkdir -p /usr/local/etc/ssl/private/
+cat /etc/ssl/certs/haproxy.crt /etc/ssl/private/haproxy.key >/usr/local/etc/ssl/private/haproxy.pem
+chown haproxy:haproxy /usr/local/etc/ssl/ -R
 # exit 1
 exec "$@"
