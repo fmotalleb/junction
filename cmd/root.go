c00a9ee9ae12fe64d89124c4b434c498d08dfa3e
FMotalleb <fmotalleb@gmail.com>
2025-06-17 09:25:57 +0330

    minor: added email to license section of cmd

--- cmd/root.go
+++ cmd/root.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2025 Motalleb Fallahnezhad
+Copyright © 2025 Motalleb Fallahnezhad (fmotalleb@gmail.com)
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
